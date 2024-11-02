324bae6be8471686836cce001f9ba6d7e10db08f
Jordan Spencer <Jordan.S.Spencer@gmail.com>
2024-11-02 17:54:11 -0400

    Update to Firebase config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDHb2jmIddvxYgBqkwO_RiFKLdhdYVhhUI',
+  authDomain: 'justbananas-ng.firebaseapp.com',
+  databaseURL: 'https://justbananas-ng-default-rtdb.firebaseio.com',
+  projectId: 'justbananas-ng',
+  storageBucket: 'justbananas-ng.firebasestorage.app',
+  messagingSenderId: '833014059147',
+  appId: '1:833014059147:web:fae97ebad6d4582ad3048e',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 @Component({
   selector: 'app-root',
